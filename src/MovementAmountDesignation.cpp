13f640a23baec5fce48738349d0d8ec1ce17a6fb
MiglyA <zq6295migly@gmail.com>
2019-07-13 23:53:12 +0900

    [update]コードスタイル変更

--- src/MovementAmountDesignation.cpp
+++ src/MovementAmountDesignation.cpp
@@ -14,65 +14,65 @@ T_Move t_move;
 
 class MovementAmountDesignation {
 private:
-	void callback(const std_msgs::Float64MultiArray::ConstPtr &msg);
-	void setOdom(const nav_msgs::Odometry::ConstPtr &odom) {t_move.setOdometry(odom);}
-	void Straight(double distance, double v);
-	void Turn(double angle, double v);
+    void callback(const std_msgs::Float64MultiArray::ConstPtr &msg);
+    void setOdom(const nav_msgs::Odometry::ConstPtr &odom) {t_move.setOdometry(odom);}
+    void Straight(double distance, double v);
+    void Turn(double angle, double v);
 
 public:
-	MovementAmountDesignation();
-	~MovementAmountDesignation();
+    MovementAmountDesignation();
+    ~MovementAmountDesignation();
 
-	ros::NodeHandle n;
-	ros::Subscriber amount;
-	ros::Subscriber odom;
-	ros::Publisher velocity;
-	ros::Publisher pub_signal;
-	ros::Publisher twist;
+    ros::NodeHandle n;
+    ros::Subscriber amount;
+    ros::Subscriber odom;
+    ros::Publisher velocity;
+    ros::Publisher pub_signal;
+    ros::Publisher twist;
 
-	double call_liner[2];
-	double call_angle[2];
-	bool move_signal = false;
-	bool move_status = false;
-	bool core_status = false;
+    double call_liner[2];
+    double call_angle[2];
+    bool move_signal = false;
+    bool move_status = false;
+    bool core_status = false;
 
 };
 
 MovementAmountDesignation::MovementAmountDesignation() {
 
-	printf("start class of 'MovementAmountDesignation'\n");
+    printf("start class of 'MovementAmountDesignation'\n");
 
-	this->odom = n.subscribe("/odom", 1000, &MovementAmountDesignation::setOdom, this);
-	this->amount = n.subscribe("/move/amount", 1000, &MovementAmountDesignation::callback, this);
-	this->twist = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1000);
-	this->velocity = n.advertise<std_msgs::Float64MultiArray>("/move/velocity", 1000);
-	this->pub_signal = n.advertise<std_msgs::Int32 >("/move/amount/signal", 1000);
+    this->odom = n.subscribe("/odom", 1000, &MovementAmountDesignation::setOdom, this);
+    this->amount = n.subscribe("/move/amount", 1000, &MovementAmountDesignation::callback, this);
+    this->twist = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1000);
+    this->velocity = n.advertise<std_msgs::Float64MultiArray>("/move/velocity", 1000);
+    this->pub_signal = n.advertise<std_msgs::Int32 >("/move/amount/signal", 1000);
 
-	call_liner[0] = 0;
-	call_liner[1] = 0;
-	call_angle[0] = 0;
-	call_angle[1] = 0;
+    call_liner[0] = 0;
+    call_liner[1] = 0;
+    call_angle[0] = 0;
+    call_angle[1] = 0;
 
 }
 
 
 MovementAmountDesignation::~MovementAmountDesignation() {
 
-	printf("shutdown class of 'MovementAmountDesignation'\n");
+    printf("shutdown class of 'MovementAmountDesignation'\n");
 }
 
 void MovementAmountDesignation::callback(const std_msgs::Float64MultiArray::ConstPtr &msg) {
 
-	//[0]:liner距離 , [1]:liner速度 , [2]:angle距離 , [3]:angle速度
-	call_liner[0] = msg->data[0];
-	call_liner[1] = msg->data[1];
-	call_angle[0] = msg->data[2];
-	call_angle[1] = msg->data[3];
+    //[0]:liner距離 , [1]:liner速度 , [2]:angle距離 , [3]:angle速度
+    call_liner[0] = msg->data[0];
+    call_liner[1] = msg->data[1];
+    call_angle[0] = msg->data[2];
+    call_angle[1] = msg->data[3];
 
-	printf("move\n");
+    printf("move\n");
 
-	t_move.resetAmount();
-	move_status = true;
+    t_move.resetAmount();
+    move_status = true;
 }
 
 void MovementAmountDesignation::Straight(double distance, double v) {
@@ -85,46 +85,46 @@ void MovementAmountDesignation::Turn(double angle, double v) {
 
 int main(int argc, char **argv) {
 
-	double v = 0;
-	double a = 0;
+    double v = 0;
+    double a = 0;
 
-	ros::init(argc, argv, "MovementAmount");
+    ros::init(argc, argv, "MovementAmount");
 
-	MovementAmountDesignation amount_move;
+    MovementAmountDesignation amount_move;
 
-	t_move.init();
+    t_move.init();
 
-	t_move.resetAmount();
+    t_move.resetAmount();
 
-	ros::Rate loop_rate(8);
+    ros::Rate loop_rate(8);
 
-	while (ros::ok()) {
+    while (ros::ok()) {
 
-		t_move.update();
+        t_move.update();
 
-		amount_move.move_signal = t_move.getMoving("straight") || t_move.getMoving("turn");
+        amount_move.move_signal = t_move.getMoving("straight") || t_move.getMoving("turn");
 
-		if (amount_move.move_status) {
-			v = t_move.exeDistance(amount_move.call_liner[0], amount_move.call_liner[1], t_move.getAmount("straight"));
-			a = t_move.exeAngle(amount_move.call_angle[0], amount_move.call_angle[1], t_move.getAmount("turn"));
-			t_move.pubVelocity(amount_move.velocity, v, 0.2, a, 1);
-			if (amount_move.move_signal) {
-				amount_move.core_status = true;
-				t_move.pubSignal(amount_move.pub_signal, 1);
-			}
-			if (amount_move.core_status && !amount_move.move_signal) {
-				amount_move.move_status = false;
-				amount_move.core_status = false;
-				t_move.pubTwist(amount_move.twist, 0, 0);
-				t_move.pubSignal(amount_move.pub_signal, 0);
-				printf("finish\n");
-			}
-		} else {
-			//t_move.pubVelocity(amount_move.velocity, 0, 0, 0, 0);
-		}
-		loop_rate.sleep();
-	}
+        if (amount_move.move_status) {
+            v = t_move.exeDistance(amount_move.call_liner[0], amount_move.call_liner[1], t_move.getAmount("straight"));
+            a = t_move.exeAngle(amount_move.call_angle[0], amount_move.call_angle[1], t_move.getAmount("turn"));
+            t_move.pubVelocity(amount_move.velocity, v, 0.2, a, 1);
+            if (amount_move.move_signal) {
+                amount_move.core_status = true;
+                t_move.pubSignal(amount_move.pub_signal, 1);
+            }
+            if (amount_move.core_status && !amount_move.move_signal) {
+                amount_move.move_status = false;
+                amount_move.core_status = false;
+                t_move.pubTwist(amount_move.twist, 0, 0);
+                t_move.pubSignal(amount_move.pub_signal, 0);
+                printf("finish\n");
+            }
+        } else {
+            //t_move.pubVelocity(amount_move.velocity, 0, 0, 0, 0);
+        }
+        loop_rate.sleep();
+    }
 
-	return 0;
+    return 0;
 
 }
--- src/MovementVelocityDesignation.cpp
+++ src/MovementVelocityDesignation.cpp
@@ -10,70 +10,77 @@
 
 T_Move tmove;
 
-class MovementVelocityDesignation {
+class MovementVelocityDesignation
+{
 public:
-	MovementVelocityDesignation();
-	~MovementVelocityDesignation();
+    MovementVelocityDesignation();
+    ~MovementVelocityDesignation();
 
-	double targetV = 0;
-	double targetA = 0;
-	double targetV_a = 0;
-	double targetA_a = 0;
+    double targetV = 0;
+    double targetA = 0;
+    double targetV_a = 0;
+    double targetA_a = 0;
 
-	ros::NodeHandle n;
-	ros::Subscriber odom;
-	ros::Subscriber amountmove;
-	ros::Publisher move;
+    ros::NodeHandle n;
+    ros::Subscriber odom;
+    ros::Subscriber amountmove;
+    ros::Publisher move;
 
-	void setOdom(const nav_msgs::Odometry::ConstPtr &odom) {tmove.setOdometry(odom);}
-	void calc(const std_msgs::Float64MultiArray::ConstPtr &msgs);
+    void setOdom(const nav_msgs::Odometry::ConstPtr &odom)
+    { tmove.setOdometry(odom); }
+    void calc(const std_msgs::Float64MultiArray::ConstPtr &msgs);
 };
 
-MovementVelocityDesignation::MovementVelocityDesignation() {
+MovementVelocityDesignation::MovementVelocityDesignation()
+{
 
-	printf("Start class of 'MovementVelocityDesignation'\n");
+    printf("Start class of 'MovementVelocityDesignation'\n");
 
-	this->odom = n.subscribe("/odom", 1000, &MovementVelocityDesignation::setOdom, this);
-	this->amountmove = n.subscribe("/move/velocity", 1000, &MovementVelocityDesignation::calc, this);
-	this->move = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1000);
+    this->odom = n.subscribe("/odom", 1000, &MovementVelocityDesignation::setOdom, this);
+    this->amountmove = n.subscribe("/move/velocity", 1000, &MovementVelocityDesignation::calc, this);
+    this->move = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1000);
 }
 
-MovementVelocityDesignation::~MovementVelocityDesignation() {
+MovementVelocityDesignation::~MovementVelocityDesignation()
+{
 
-	printf("Shutdown class of 'MovementVelocityDesignation'\n");
+    printf("Shutdown class of 'MovementVelocityDesignation'\n");
 }
 
-void MovementVelocityDesignation::calc(const std_msgs::Float64MultiArray::ConstPtr &msgs) {
+void MovementVelocityDesignation::calc(const std_msgs::Float64MultiArray::ConstPtr &msgs)
+{
 
-	//[0]: 速度, [1]: 加速度, [2]: 角速度, [3]: 角加速度
-	this->targetV = msgs->data[0];
-	this->targetV_a = msgs->data[1];
-	this->targetA = msgs->data[2];
-	this->targetA_a = msgs->data[3];
+    //[0]: 速度, [1]: 加速度, [2]: 角速度, [3]: 角加速度
+    this->targetV = msgs->data[0];
+    this->targetV_a = msgs->data[1];
+    this->targetA = msgs->data[2];
+    this->targetA_a = msgs->data[3];
 }
 
+int main(int argc, char **argv)
+{
 
-int main(int argc, char **argv) {
+    ros::init(argc, argv, "MovementVelocity");
 
-	ros::init(argc, argv, "MovementVelocity");
+    MovementVelocityDesignation amount;
 
-	MovementVelocityDesignation amount;
+    tmove.init();
 
-	tmove.init();
+    ros::Rate loop_rate(8);
 
-	ros::Rate loop_rate(8);
+    tmove.resetAmount();
 
-	tmove.resetAmount();
+    while (ros::ok()) {
 
-	while (ros::ok()) {
+        tmove.update();
 
-		tmove.update();
+        tmove.pubTwist(amount.move,
+                       tmove.calcVelocityStraight(amount.targetV_a, amount.targetV),
+                       tmove.calcVelocityTurn(amount.targetA_a, amount.targetA));
 
-		tmove.pubTwist(amount.move, tmove.calcVelocityStraight(amount.targetV_a, amount.targetV), tmove.calcVelocityTurn(amount.targetA_a, amount.targetA));
+        loop_rate.sleep();
 
-		loop_rate.sleep();
+    }
 
-	}
-
-	return 0;
+    return 0;
 }
