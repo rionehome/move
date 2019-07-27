2ac9756547e569d305478373f5461f1a93a6b7a1
MiglyA <zq6295migly@gmail.com>
2019-07-27 23:13:07 +0900

    [update]update関数作成開始

--- src/velocity.cpp
+++ src/velocity.cpp
@@ -1,52 +1,72 @@
 //
 // Created by migly-home on 19/07/26.
 //
-
+#include "ros/ros.h"
 #include <geometry_msgs/Twist.h>
+#include "move/Velocity.h"
 #include <nav_msgs/Odometry.h>
 #include "../include/move/velocity.h"
-#include "../include/move/MovementVelocityDesignation_H.h"
+
+#define MAX_LINEAR 0.7
+#define MAX_ANGULAR 110
 
 Velocity::Velocity(ros::NodeHandle *n)
 {
     printf("Start class of 'Velocity'\n");
-    this->odom = n->subscribe("/odom", 1000, &Velocity::setOdometry, this);
-    this->velocity = n->subscribe("/move/velocity", 1000, &Velocity::callbackVelocity, this);
-    this->twist = n->advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1000);
+    this->velocity_sub = n->subscribe("/move/velocity", 1000, &Velocity::callbackVelocity, this);
+    this->twist_pub = n->advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1000);
 }
+
 Velocity::~Velocity()
 {
     printf("Shutdown class of 'Velocity'\n");
 }
 
-void Velocity::setOdometry(const nav_msgs::Odometry::ConstPtr &msg)
+void Velocity::publishTwist(double liner_x, double angular_z)
 {
-    this->odometry.x = msg->pose.pose.position.x;
-    this->odometry.y = msg->pose.pose.position.y;
-    this->odometry.z = msg->pose.pose.position.z;
-    this->odometry.angular_x = msg->pose.pose.orientation.x;
-    this->odometry.angular_y = msg->pose.pose.orientation.y;
-    this->odometry.angular_z = msg->pose.pose.orientation.z;
-    this->odometry.angular_w = msg->pose.pose.orientation.w;
-    this->odometry.linear_speed = msg->twist.twist.linear.x;
-    this->odometry.angular_speed = msg->twist.twist.angular.z;
-    this->odometry.enable = true;
+    geometry_msgs::Twist twist = geometry_msgs::Twist();
+    twist.linear.x = liner_x;
+    twist.linear.y = 0.0;
+    twist.linear.z = 0.0;
+    twist.angular.x = 0.0;
+    twist.angular.y = 0.0;
+    twist.angular.z = angular_z;
+    this->twist_pub.publish(twist);
 }
 
-void Velocity::callbackVelocity(const nav_msgs::Odometry::ConstPtr &msg)
+void Velocity::callbackVelocity(const move::Velocity::ConstPtr &msg)
 {
+    /*
+     * 直進、回転の加速度と速度の情報を受け取り.
+     * linear:0.7m/sが最大.
+     * angular:110deg/sが最大.
+     */
+    this->last_linear_acceleration = msg->linear_acceleration_rate * MAX_LINEAR;
+    this->last_linear = msg->linear_rate * MAX_LINEAR;
+    this->last_angular_acceleration = msg->angular_acceleration_rate * MAX_ANGULAR;
+    this->last_angular = msg->angular_rate * MAX_ANGULAR;
+}
 
+void Velocity::velocity_update()
+{
+    /*
+     * linear:0.7m/sが最大.
+     * angular:110deg/sが最大.
+     */
+
+    this->publishTwist(this->stack_linear, this->stack_angular);
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "Velocity");
     ros::Rate loop_rate(10);
+    ros::NodeHandle n;
+    Velocity velocity = Velocity(&n);
     while (ros::ok()) {
-
         ros::spinOnce();
+        velocity.velocity_update();
         loop_rate.sleep();
     }
-
     return 0;
 }
