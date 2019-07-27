0c199666ce037a0972ea357f33d12e364d3e63c7
MiglyA <zq6295migly@gmail.com>
2019-07-27 23:12:52 +0900

    [add]msg追加

--- include/move/velocity.h
+++ include/move/velocity.h
@@ -1,8 +1,6 @@
 //
 // Created by migly-home on 19/07/26.
 //
-#include "ros/ros.h"
-
 #ifndef VELOCITY_H
 #define VELOCITY_H
 class Velocity
@@ -10,27 +8,22 @@ class Velocity
 public:
     explicit Velocity(ros::NodeHandle *n);
     ~Velocity();
+    void velocity_update();
 
-    ros::Subscriber odom;
-    ros::Subscriber velocity;
-    ros::Publisher twist;
-
-    void setOdometry(const nav_msgs::Odometry_<std::allocator<void>>::ConstPtr &msg);
 private:
-    typedef struct
-    {
-        double x;
-        double y;
-        double z;
-        double angular_x;
-        double angular_y;
-        double angular_z;
-        double angular_w;
-        double linear_speed;
-        double angular_speed;
-        bool enable = false;
-    } Odometry;
-    Odometry odometry;
+    ros::Subscriber velocity_sub;
+    ros::Publisher twist_pub;
+    double stack_linear = 0.0;
+    double stack_angular = 0.0;
+    double last_linear = 0.0;
+    double last_linear_acceleration = 0.0;
+    double last_angular = 0.0;
+    double last_angular_acceleration = 0.0;
+
+    void callbackVelocity(const move::Velocity_<std::allocator<void>>::ConstPtr &msg);
+    void publishTwist(double liner_x, double angular_z);
+    double toRadian(double angle)
+    { return (angle * M_PI) / 180; }
 };
 
 
